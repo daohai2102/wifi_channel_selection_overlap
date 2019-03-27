05b002c9430dbcc5f2cd0780f4cd07d7daf14409
Dao Duy Hai <duyhai.onlysea@gmail.com>
2019-03-27 17:00:11 +0700

    libssh wrapper for running commands remotely

---
+++ SshSession.cc
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <stdlib.h>
+
+#include "SshSession.h"
+
+SshSession::SshSession(){
+	fprintf(stderr, "Entering SshSession::SshSession()\n");
+	mySshSession = ssh_new();
+	if (!mySshSession){
+		fprintf(stderr, "Error creating ssh session, exiting ...\n");
+		exit(1);
+	}
+	connected = false;
+	fprintf(stderr, "Leaving SshSession::SshSession()\n");
+}
+
+SshSession::~SshSession(){
+	fprintf(stderr, "Entering SshSession::~SshSession()\n");
+	if (connected)
+		ssh_disconnect(mySshSession);
+	ssh_free(mySshSession);
+	fprintf(stderr, "Leaving SshSession::~SshSession()\n");
+}
+
+SshSession& SshSession::setPrivateKeyFile(const char *filename){
+	strcpy(privateKeyFile, filename);
+	return *this;
+}
+
+SshSession& SshSession::setUserName(const char *name){
+	strcpy(userName, name);
+	return *this;
+}
+
+SshSession& SshSession::setAddress(const char *addr){
+	strcpy(address, addr);
+	return *this;
+}
+
+SshSession& SshSession::setPort(int p){
+	port = p;
+	return *this;
+}
+
+SshSession& SshSession::setVerbosity(int verbo){
+	ssh_options_set(mySshSession, SSH_OPTIONS_LOG_VERBOSITY, &verbo);
+	return *this;
+}
+
+SshSession& SshSession::connectSsh(){
+	ssh_options_set(mySshSession, SSH_OPTIONS_HOST, address);
+	ssh_options_set(mySshSession, SSH_OPTIONS_PORT, &port);
+	ssh_options_set(mySshSession, SSH_OPTIONS_USER, userName);
+
+	int rc = ssh_connect(mySshSession);
+	if (rc != SSH_OK){
+		throw SshConnectException(ssh_get_error(mySshSession));
+	}
+	connected = true;
+
+	rc = ssh_userauth_privatekey_file(mySshSession, NULL, privateKeyFile, NULL);
+	if (rc != SSH_AUTH_SUCCESS){
+		throw SshUserAuthException(ssh_get_error(mySshSession));
+	}
+
+	return *this;
+}
+
+std::string SshSession::runCommand(const char *cmd){
+	ssh_channel channel = ssh_channel_new(mySshSession);
+	if (!channel){
+		throw SshNewChannelException(ssh_get_error(mySshSession));
+	}
+
+	int rc = ssh_channel_open_session(channel);
+	if (rc != SSH_OK){
+		ssh_channel_free(channel);
+		throw SshChannelOpenSessionException(ssh_get_error(mySshSession));
+	}
+
+	rc = ssh_channel_request_exec(channel, cmd);
+	if (rc != SSH_OK){
+		ssh_channel_close(channel);
+		ssh_channel_free(channel);
+		throw SshExecCommandException(ssh_get_error(mySshSession));
+	}
+
+	char buffer[256];
+	std::string response;
+	unsigned int nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
+	while (nbytes > 0){
+		std::cerr << "read " << nbytes << "bytes" << std::endl;
+		response.append(buffer, nbytes);
+		nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
+	}
+
+	if (nbytes < 0){
+		ssh_channel_close(channel);
+		ssh_channel_free(channel);
+		throw SshChannelReadException(ssh_get_error(mySshSession));
+	}
+
+	ssh_channel_send_eof(channel);
+	ssh_channel_close(channel);
+	ssh_channel_free(channel);
+	return response;
+}
---
+++ SshSession.h
@@ -0,0 +1,116 @@
+#ifndef _SSH_SESSION_H_
+#define _SSH_SESSION_H_
+
+#include <libssh/libssh.h>
+#include <exception>
+#include <cstring>
+#include <string>
+
+class SshSession{
+private:
+	char privateKeyFile[256];
+	char userName[256];
+	char address[256];
+	int port;
+	ssh_session mySshSession;
+	bool connected;
+public:
+	SshSession();
+	~SshSession();
+	std::string runCommand(const char *cmd);
+	SshSession& setUserName(const char *name);
+	SshSession& setAddress(const char *addr);
+	SshSession& setPort(int p);
+	SshSession& setVerbosity(int verbo);
+	SshSession& setPrivateKeyFile(const char *filename);
+	SshSession& connectSsh();
+};
+
+class SshConnectException : public std::exception {
+private:
+	char message[256];
+public:
+	SshConnectException(){
+		strcpy(message, "Error connecting\0");
+	}
+	SshConnectException(const char *mess) {
+		strcpy(message, "Error connecting: \0");
+		strcpy(message + strlen(message), mess);
+	};
+	const char *what() const throw(){
+		return message;
+	}
+};
+
+class SshUserAuthException : public std::exception {
+private:
+	char message[256];
+public:
+	SshUserAuthException(){
+		strcpy(message, "Error user authenticating\0");
+	}
+	SshUserAuthException(const char *mess){
+		strcpy(message, "Error user authenticating: \0");
+		strcpy(message + strlen(message), mess);
+	}
+	const char *what() const throw(){
+		return message;
+	}
+};
+
+class SshNewChannelException : public std::exception {
+private:
+	char message[256];
+public:
+	SshNewChannelException(const char *mess){
+		strcpy(message, "Error creating ssh channel: \0");
+		strcpy(message + strlen(message), mess);
+	}
+	const char *what() const throw(){
+		return message;
+	}
+};
+
+class SshChannelOpenSessionException : public std::exception{
+private:
+	char message[256];
+public:
+	SshChannelOpenSessionException(){
+		strcpy(message, "Error ssh_channel_open_session\0");
+	}
+	SshChannelOpenSessionException(const char *mess){
+		strcpy(message, "Error ssh_channel_open_session: \0");
+		strcpy(message + strlen(message), mess);
+	}
+	const char *what() const throw(){
+		return message;
+	}
+};
+
+class SshExecCommandException : public std::exception{
+private:
+	char message[256];
+public:
+	SshExecCommandException(const char *mess){
+		strcpy(message, "Error executing command: \0");
+		strcpy(message + strlen(message), mess);
+	}
+	const char *what() const throw(){
+		return message;
+	}
+};
+
+class SshChannelReadException : public std::exception{
+private:
+	char message[256];
+public:
+	SshChannelReadException(const char *mess){
+		strcpy(message, "Error reading ssh response: \0");
+		strcpy(message + strlen(message), mess);
+	}
+	const char *what() const throw(){
+		return message;
+	}
+};
+
+#endif
